395e299f5d83eedbebf0738760a86d51b0db97ab
Manish Pardhan <manish@instaastro.in>
2025-07-31 13:00:49 +0530

    transation on terra blockchain

---
+++ swap.js
@@ -0,0 +1,37 @@
+const { LCDClient, MnemonicKey, MsgSend, isTxError, Coin, MsgSwap } = require('@terra-money/terra.js')
+require('dotenv').config();
+
+const main = async () => {
+    const terra = new LCDClient({
+        URL: process.env.TERRA_NODE_URL,
+        chainID: process.env.TERRA_CHAIN_ID,
+    })
+    const mk = new MnemonicKey({
+        mnemonic: process.env.MENMONIC
+    });
+
+    const wallet = terra.wallet(mk);
+    const toAddress = "terravaloper1kfnw4h0zv6pz0x2apwsmu2zty30jrz2ctzvtld";
+    const msg = new MsgSwap(wallet.key.accAddress, toAddress, new Coin("uluna", "1000000"), "uusd");
+
+    const tx = await wallet.createAndSignTx({
+        msgs: [msg],
+        memo: "This is the transaction",
+    })
+
+    const txHash = await terra.tx.hash(tx);
+    console.log("txHash", txHash);
+    const txResult = await terra.tx.broadcast(tx);
+    if(isTxError(txResult)) {
+        console.log(txResult);
+        throw new Error();
+    }
+
+    console.log(`logs: ${txResult.logs}`)
+}
+
+main().then((res) => {
+    console.log(res);
+}).catch((err)=>{
+    console.log(err);
+})
